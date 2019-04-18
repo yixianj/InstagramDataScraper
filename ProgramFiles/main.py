fff02db32ebf04e275fcd5c74d5a6029df404a69
yixianj <yixianj@umich.edu>
2019-04-18 17:12:50 -0400

    Fully debugged. Printing must be adjusted for missing column values

--- ProgramFiles/main.py
+++ ProgramFiles/main.py
@@ -10,7 +10,7 @@ import datetime
 
 ##### Global variables #####
 # Dictionary of input data, read from file
-scraperInput = {}
+# scraperInput = {}
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
 env.trim_blocks = True
@@ -24,7 +24,7 @@ insertRawUserDataP1 = """INSERT INTO raw_user_data(user_data) VALUES (' """
 insertRawUserDataP2 = """');"""
 updateUserTemp = env.get_template('updateUserTable.sql')
 
-def updateUserAndPostData(cur, conn, userData, searchId):
+def updateUserAndPostData(cur, conn, scraperInput, userData, searchId):
     print("UPDATING USER DATA")
     cur.execute("SELECT search FROM ig_users WHERE id = " + userData["id"] + ";")
     search = cur.fetchone()[0]
@@ -88,12 +88,19 @@ def instagramDataScraper():
 
     for user in users:
         userData = extractData.getUserData(user)
-        userData["search"] = [searchId]
-        if (scraperInput["search"]["email"] != ""):
-            userData["email"] = clean.extractEmails(userData["biography"], scraperInput["search"]["email"])
-        userData["num_posts"] = user["edge_owner_to_timeline_media"]["count"]
-        userData["full_name"] = clean.removeEmojisAndOther(userData["full_name"])
-        userData["biography"] = clean.removeEmojisAndOther(userData["biography"])
+
+        try:
+            userData["search"] = [searchId]
+            if (scraperInput["search"]["email"] != ""):
+                userData["email"] = clean.extractEmails(userData["biography"], scraperInput["search"]["email"])
+            userData["num_posts"] = user["edge_owner_to_timeline_media"]["count"]
+            userData["full_name"] = clean.removeEmojisAndOther(userData["full_name"])
+            userData["biography"] = clean.removeEmojisAndOther(userData["biography"])
+        except LookupError as lookupErr:
+            print("LookupError: could not find key or index.")
+            print(lookupErr)
+            print("### userData ###\n" + str(userData))
+            pass
 
         insertUserQuery = insertUserTemp.render(userData=userData)
         try:
@@ -102,7 +109,7 @@ def instagramDataScraper():
             print("errorPsycopg2: duplicateErr")
             print(duplicateErr)
             conn.rollback()
-            updateUserAndPostData(cur, conn, userData, searchId)
+            updateUserAndPostData(cur, conn, scraperInput, userData, searchId)
             pass
         except psycopg2.InternalError as internalErr:
             print("errorPsycopg2: internalErr")
@@ -116,13 +123,21 @@ def instagramDataScraper():
 
         edges = user["edge_owner_to_timeline_media"]["edges"]
         for item in edges:
-            postData = extractData.getPostData(item["node"])
-            postData["location"] = json.dumps(postData["location"])
-            postData["thumbnail_resources"] = json.dumps(postData["thumbnail_resources"])
-            postData["edge_media_to_caption"] = clean.removeEmojisAndOther(postData["edge_media_to_caption"]
-                                                                ["edges"][0]["node"]
-                                                                ["text"])
+            try:
+                postData = extractData.getPostData(item["node"])
+                postData["location"] = json.dumps(postData["location"])
+                postData["thumbnail_resources"] = json.dumps(postData["thumbnail_resources"])
+                postData["edge_media_to_caption"] = clean.removeEmojisAndOther(postData["edge_media_to_caption"]
+                                                                    ["edges"][0]["node"]
+                                                                    ["text"])
+            except LookupError as lookupErr:
+                print("LookupError: key or index does not exist.")
+                print(lookupErr)
+                print("### postData ###\n" + str(postData))
+                pass
+
             insertPostQuery = insertPostTemp.render(postData = postData)
+
             try:
                 cur.execute(insertPostQuery)
             except psycopg2.IntegrityError as duplicateErr:
